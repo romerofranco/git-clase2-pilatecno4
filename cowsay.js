f8a831df6e8d775ba033942e6661b3104f8d7362
FrancoRomero <romerofranco.168@gmail.com>
2023-06-23 12:40:13 -0300

    feature: added cow message

--- cowsay.js
+++ cowsay.js
@@ -1,1 +1,8 @@
-console.log('WIP Cowsay-project'); 
+const cowsay = require("cowsay");
+
+const cow = cowsay.say({
+  text: "Hola PilarTecno #4, soy una vaca",
+  e: "oO",
+  T: "U ",
+});
+console.log(cow);
